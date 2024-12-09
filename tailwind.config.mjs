e2fc0a55b7aa9e9979b3f250e8000c60e382c29f
Elvis Crespo <eacresposu@gmail.com>
2024-12-08 23:58:12 -0500

    add change landing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,55 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			sans: ['Gotham SSm A', 'sans-serif'],
-		},
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Gotham SSm A", "sans-serif"],
+    },
+    extend: {
+      // colors: {
+      //   primary: "#ff7d12",
+      // },
+      // screens: {
+      //   sm: "640px",
+      //   md: "768px",
+      //   lg: "1024px",
+      //   xl: "1280px",
+      //   "2xl": "1536px",
+      // },
+      // keyframes: {
+      //   "accordion-down": {
+      //     from: { height: "0" },
+      //     to: { height: "var(--radix-accordion-content-height)" },
+      //   },
+      //   "accordion-up": {
+      //     from: { height: "var(--radix-accordion-content-height)" },
+      //     to: { height: "0" },
+      //   },
+      // },
+      // animation: {
+      //   "accordion-down": "accordion-down 0.2s ease-out",
+      //   "accordion-up": "accordion-up 0.2s ease-out",
+      // },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",  
+      // },      
+    },
+  },
+  plugins: [
+
+	//add scrollbar thin
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".scrollbar-thin": {
+          "scrollbar-width": "thin",
+        },
+      });
+    }),
+
+  ],
+};
